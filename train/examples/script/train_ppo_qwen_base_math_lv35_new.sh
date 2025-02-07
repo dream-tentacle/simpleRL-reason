be4cb034c629546d699c27f71c494370f5fd0494
dream <221240060@smail.nju.edu.cn>
2025-02-07 08:17:21 +0800

    改了自己的文件

--- train/examples/script/train_ppo_qwen_base_math_lv35_new.sh
+++ train/examples/script/train_ppo_qwen_base_math_lv35_new.sh
@@ -1,10 +1,10 @@
 
-HDFS_HOME=TO_BE_DEFINED
-RUN_NAME=Qwen2.5-Math-7B_ppo_from_base_math_lv35
+HDFS_HOME=/home/nfs05/xiongjc/model
+RUN_NAME=Llama3.2-3b-instruct-train
 
-python3 openrlhf/cli/train_ppo_ray_box.py \
+CUDA_VISIBLE_DEVICES=0,4 python openrlhf/cli/train_ppo_ray_box.py \
     --ref_num_nodes 1 \
-    --ref_num_gpus_per_node 8 \
+    --ref_num_gpus_per_node 2 \
     --reward_num_nodes 0 \
     --reward_num_gpus_per_node 0 \
     --critic_num_nodes 1 \
@@ -14,7 +14,7 @@ python3 openrlhf/cli/train_ppo_ray_box.py \
     --vllm_num_engines 16 \
     --vllm_tensor_parallel_size 1 \
     --colocate_actor_ref \
-    --pretrain $HDFS_HOME/model_hub/models--Qwen--Qwen2.5-Math-7B/snapshots/b101308fe89651ea5ce025f25317fea6fc07e96e \
+    --pretrain $HDFS_HOME/Llama3.2-3b-instruct \
     --save_path $HDFS_HOME/checkpoints/$RUN_NAME \
     --micro_train_batch_size 2 \
     --train_batch_size 128 \
@@ -32,14 +32,14 @@ python3 openrlhf/cli/train_ppo_ray_box.py \
     --actor_learning_rate 5e-7 \
     --critic_learning_rate 9e-6 \
     --init_kl_coef 0.01 \
-    --prompt_data  data/math_level3to5_data_processed_with_qwen_prompt.json \
-    --input_key input \
+    --prompt_data  ~/ReFT/data/GSM8k/train.json \
+    --input_key question \
     --normalize_reward \
     --flash_attn \
     --gradient_checkpointing \
     --save_steps 4 \
     --load_checkpoint \
-    --use_wandb YOUR_WANDB_KEY \
+    --use_wandb 15dbd82bdbd8aa48b934305e9ddcff2229e64c29 \
     --wandb_run_name $RUN_NAME \
     --ckpt_path $HDFS_HOME/checkpoints/$RUN_NAME  \
     --max_ckpt_num 20000
